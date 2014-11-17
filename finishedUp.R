87d1ca6fe13d2f1c345ecdedb887512e0ccc3690
Jonah Sinick <jsinick@gmail.com>
2014-11-17 11:34:44 -0800

    Still working

--- finishedUp.R
+++ finishedUp.R
@@ -14,10 +14,12 @@ library(Metrics)
 
 myTrain = read.csv('~/Desktop/speedDating/currentTrain.csv')
 myTest = read.csv('~/Desktop/speedDating/currentTest.csv')
+
+myTrainTemp = myTrain
+
 myTrain = myTrain[,colSums(myTrain^2) != 0 & colSums(myTest^2) != 0]
-myTest = myTest[,colSums(myTrain^2) != 0 & colSums(myTest^2) != 0]
-
-
+myTest = myTest[,colSums(myTrainTemp^2) != 0 & colSums(myTest^2) != 0]
+myTest = myTest[,colSums(myTrainTemp^2) != 0 & colSums(myTest^2) != 0]
 getProbs = function(train, test, features, tar){
   s=scale(train[features],center=TRUE,scale=TRUE)
   co=heuristicC(s)
@@ -30,106 +32,104 @@ getProbs = function(train, test, features, tar){
 
 
 
-addProbCol = function(train, test, colName, features, tar){
-  print(colName)
-  outputHash = hash()
+addProbColsGender = function(genderHash){
+  train = genderHash[["train"]]
+  test = genderHash[["test"]]
+  tars = c("dec_M","dec_W")
+  colNames = genderHash[["colNames"]]
+  menFeatures = genderHash[["featuresMen"]]
+  womenFeatures = genderHash[["featuresWomen"]]
+  outputHash  = addProbCol(genderHash, colNames[1], menFeatures, tars[1], 4)
+  outputHash  = addProbCol(genderHash, colNames[2], womenFeatures, tars[2], 4)
+  return(outputHash)
+}
+
+addProbCol = function(dfHash, colName, features, tar, precision){
+  train = dfHash[["train"]]
+  test = dfHash[["test"]]
   len = length(features)
+  finished = FALSE
   currentFeatures = c()
   bestLogLoss = 100
   for(i in 1:len){
+    print("HERE")
+    print(i)
+    print(currentFeatures)
+    betterFound = FALSE
     lenNew = length(currentFeatures) + 1
+    tempBestLogLoss = BestLogLoss
     for(f in features){
       tempFeatures =  c(currentFeatures, f)
-      probs = getProbs(train, test, tempFeatures, tar)
-      newLogLoss = logLoss(test[[tar]],probs)
-      if(newLogLoss < bestLogLoss){
+      probs = getProbs(train, train, tempFeatures, tar)
+      newLogLoss = logLoss(train[[tar]],probs)
+      if(round(newLogLoss,precision) < round(bestLogLoss),precision){
         bestLogLoss = newLogLoss
-        test[[colName]] = probs
-        currentFeatures = tempFeatures
+        print(newLogLoss)
+        currentBestFeatures = tempFeatures
+        betterFound = TRUE
       }
     }
-    if(len + 1 >= lenNew | i == len){
-      outputHash[["newTest"]] = test
-      outputHash[["features"]] = currentFeatures
-      return(outputHash)
-    }
+    currentFeatures = currentBestFeatures
+    if(betterFound == FALSE | i == len){
+      train[[colName]] = probs
+      test[[colName]] = getProbs(train, test, currentFeatures, tar)
+      dfHash[["train"]] = train
+      dfHash[["test"]] = test
+      dfHash[["features"]] = currentFeatures
+      return(dfHash)
+    } 
   }
 }
 
-createProbs = function(train, test, featureHash, base, tar){
-  TCBHash = hash()
-  for(colName in keys(featureHash)){
-    features = c(base, featureHash[[colName]])
-    outputHash = addProbCol(train, test, colName, features, tar)
-    test = outputHash[["newTest"]]
-    outputHash = del("newTest", outputHash)
-    TCBHash[[colName]] = outputHash
-  }
-  TCBHash[["newTest"]] = test
-  return(TBCHash)
-}
-
-
-
-
-decAvgOnMan = hash()
 
 
 Names = names(myTrain)
-Names = Names[!(Names %in% Names[grep("Sum|Guess|Rating_W_of_M|gender_M|Rating_M_of_W|genderRating_W_of_M|order|goal_M|goal_W|prob_M|prob_W|Cross|iid_W|id_W|wave_W|dec_W|dec_M",Names)])]
+Names = Names[!(Names %in% Names[grep("Sum|Guess|Rating_W_of_M|gender_M|Rating_M_of_W|genderRating_W_of_M|order|goal_M|goal_W|prob_M|prob_W|Cross|iid_W|id_W|wave_W|dec_W|dec_M|partner_W|pid_W",Names)])]
 menNames = Names[grep("W_of_M|_M$", Names)]
 womenNames = Names[grep("M_of_W|_W$", Names)]
+guessNames = Names[grep("Guess$", Names)]
+crossNames = Names[grep("Cross", Names)]
+menDecAvgs = c("decAvg_M_of_W", "raterDecAvg_M") 
+womenDecAvgs = c("decAvg_W_of_M", "raterDecAvg_W") 
 
-setScore = function(train,test,colName,features,target){
-  answerHash = hash()
-  answerHash[["newTrain"]] = addProbCol(train, train,  colName , features, target)[["newTest"]]
-  answerHash[["newTest"]] = addProbCol(train, test,  colName , features, target)[["newTest"]]
-  return(answerHash)
-}
+genderHash = hash()
+genderHash[["train"]] = myTrain
+genderHash[["test"]] = myTest
 
+genderHash[["colNames"]] = c("womanRateeScore","manRateeScore")
+genderHash[["featuresMen"]] = womenNames
+genderHash[["featuresWomen"]] = menNames
+genderHash = addProbColsGender(genderHash)
 
+genderHash[["colNames"]] = c("manRaterScore","womanRaterScore")
+genderHash[["featuresMen"]] = menNames
+genderHash[["featuresWomen"]] = womenNames
 
+genderHash = addProbColsGender(genderHash)
 
-myTest =  addProbCol(myTrain, myTest,  "manRateeScore" , menNames, "dec_W")[["newTest"]]
-myTest = addProbCol(myTrain, myTest,  "womanRateeScore" , womenNames, "dec_M")[["newTest"]]
-myTest = addProbCol(myTrain, myTest,  "manRaterScore" , menNames, "dec_M")[["newTest"]]
-myTest =  addProbCol(myTrain, myTest,  "womanRaterScore" , womenNames, "dec_W")[["newTest"]]
-
-myTest =  addProbCol(myTrain, myTest,  "simpleManRateeScore" ,c("decAvg_M_of_W"), "dec_W")[["newTest"]]
-myTest =  addProbCol(myTrain, myTest,  "simpleWomanRateeScore" , c("decAvg_W_of_M"), "dec_M")[["newTest"]]
-myTest =  addProbCol(myTrain, myTest,  "simpleManRaterScore" ,c("raterDecAvg_M"), "dec_M")[["newTest"]]
-myTest =  addProbCol(myTrain, myTest,  "simpleWomanRaterScore" , c("raterDecAvg_W"), "dec_W")[["newTest"]]
-
-myTrain =  addProbCol(myTrain, myTrain,  "manRateeScore" , menNames, "dec_W")[["newTest"]]
-myTrain = addProbCol(myTrain, myTrain,  "womanRateeScore" , womenNames, "dec_M")[["newTest"]]
-myTrain = addProbCol(myTrain, myTrain,  "manRaterScore" , menNames, "dec_M")[["newTest"]]
-myTrain =  addProbCol(myTrain, myTrain,  "womanRaterScore" , womenNames, "dec_W")[["newTest"]]
-
-myTrain =  addProbCol(myTrain, myTrain,  "simpleManRateeScore" ,c("decAvg_M_of_W"), "dec_W")[["newTest"]]
-myTrain =  addProbCol(myTrain, myTrain,  "simpleWomanRateeScore" , c("decAvg_W_of_M"), "dec_M")[["newTest"]]
-myTrain =  addProbCol(myTrain, myTrain,  "simpleManRaterScore" ,c("raterDecAvg_M"), "dec_M")[["newTest"]]
-myTrain =  addProbCol(myTrain, myTrain,  "simpleWomanRaterScore" , c("raterDecAvg_W"), "dec_W")[["newTest"]]
-
-myTest =  addProbCol(myTrain, myTest,  "simpleDec_M_Score" ,c("decAvg_M_of_W", "raterDecAvg_M"), "dec_M")[["newTest"]]
-myTest =  addProbCol(myTrain, myTest,  "simpleDec_W_Score" ,c("decAvg_W_of_M", "raterDecAvg_W"), "dec_W")[["newTest"]]
-
-myTrain =  addProbCol(myTrain, myTrain,  "simpleDec_M_Score" ,c("decAvg_M_of_W", "raterDecAvg_M"), "dec_M")[["newTest"]]
-myTrain =  addProbCol(myTrain, myTrain,  "simpleDec_W_Score" ,c("decAvg_W_of_M", "raterDecAvg_W"), "dec_W")[["newTest"]]
-
-myTest =  addProbCol(myTrain, myTest,  "enhancedDec_M_Score" ,c("decAvg_M_of_W", "raterDecAvg_M", "womanRateeScore","manRaterScore"), "dec_M")[["newTest"]]
-myTest =  addProbCol(myTrain, myTest,  "enhancedDec_W_Score" ,c("decAvg_W_of_M", "raterDecAvg_W", "manRateeScore","womanRaterScore"), "dec_W")[["newTest"]]
-
-myTrain =  addProbCol(myTrain, myTrain,  "enhancedDec_M_Score" ,c("decAvg_M_of_W", "raterDecAvg_M", "womanRateeScore","manRaterScore"), "dec_M")[["newTest"]]
-myTrain =  addProbCol(myTrain, myTrain,  "enhancedDec_W_Score" ,c("decAvg_W_of_M", "raterDecAvg_W", "manRateeScore","womanRaterScore"), "dec_W")[["newTest"]]
+genderHash[["colNames"]] = c("simpleDecManScore","simpleDecWomanScore")
+genderHash[["featuresMen"]] = menDecAvgs
+genderHash[["featuresWomen"]] = womenDecAvgs
+genderHash = addProbColsGender(genderHash)
 
 
+currentFeatures = c(menNames, womenNames, "womanRateeScore", "manRateeScore", 
+                     "womanRaterScore", "manRaterScore", "simpleDecManScore", "simpleDecWomanScore")
+genderHash[["colNames"]] = c("decManScoreNoInters", "decWomanScoreNoInters")
+genderHash[["featuresMen"]] = currentFeatures
+genderHash[["featuresWomen"]] = currentFeatures
 
-myTrain = myTrain[1:284]
+train = genderHash[["train"]]
+test = genderHash[["test"]]
+train[["naiveMatchScore"]] = train[["simpleDecManScore"]]*train[["simpleDecWomanScore"]]
+test[["naiveMatchScore"]] = test[["simpleDecManScore"]]*test[["simpleDecWomanScore"]]
+train[["sophMatchScore"]] = train[["decManScoreNoInters"]]*train[["decManScoreNoInters"]]
+test[["sophMatchScore"]] = test[["decWomanScoreNoInters"]]*test[["decWomanScoreNoInters"]]
+genderHash[["train"]] = train
+genderHash[["test"]] = test
 
-myTest["matchUsingSimpleRateeScores"] = myTest["decAvg_W_of_M"]*myTest["raterDecAvg_W"]
-myTest["dec_M_UsingDecAvgs"] = myTest["decAvg_M_of_W"]*myTest["raterDecAvg_M"]
-
-
+genderHash = addProbCol(genderHash, "simpleMatchScore", c("naiveMatchScore", menDecAvgs, womenDecAvgs), "match")
+genderHash = addProbCol(genderHash, "matchScoreNoInters", c(currentFeatures, "naiveMatchScore", "sophMatchScore", "simpleMatchScore"), "match")
 
 
 h = function(df, tar,probs){
@@ -147,14 +147,26 @@ h = function(df, tar,probs){
   return(tab)
 }
 
-for(n in c("simpleDec_M_Score", "simpleManRateeScore","womanRaterScore","simpleWomanRaterScore")){
-  print(n)
-  h(myTrain, "dec_W", n)
+orderedFeatures = c("decAvg_M_of_W",         "attrRatingAvg_M_of_W",  "met_W",                 "fieldScience_W",        "income_W",             
+  "careerPsychology_W",    "exerciseAct_W",         "theaterAct_W",          "goalOther_W",           "fieldAcademia_W",      
+  "sportsAct_W",           "go_out2_W",             "careerInternational_W", "yogaAct_W",             "sincPref_W",           
+  "intelRatingAvg_M_of_W", "likeRatingAvg_M_of_W",  "careerCreative_W",      "goalGetDate_W",         "gamingAct_W",          
+  "race_W",                "moviesAct_W",           "tvAct_W",               "shoppingAct_W",         "careerAcademic_W",     
+  "date7_W",               "fieldSocialWork_W",     "date2_W",               "go_out1_W",             "careerSocialWork_W",   
+  "artAct_W",              "goalSeriousRel_W",      "fieldHistReligPhil_W",  "funRatingAvg_M_of_W",   "fieldMath_W",          
+  "careerMedicine_W",      "intelPref_W",           "museumsAct_W",          "funPref_W",             "fieldEngin_W",         
+  "hikingAct_W",           "go_out_W",              "raceWhite_W",           "fieldEnglish_W",        "fieldSocialSci_W",     
+  "tuition_W",             "exphappy_W",            "goalFunNight_W",        "attrRatingAvg_M_of_W",  "date3_W",              
+  "samerace_W",            "readingAct_W",          "activityAvg_W",         "sincRatingAvg_M_of_W",  "fieldLaw_W",           
+  "decAvg_M_of_W",         "go_out4_W",             "go_out3_W",             "sharRatingAvg_M_of_W",  "fieldBusiness_W",      
+  "concertsAct_W",         "imprace_W",             "raceAsian_W",           "go_out3_W",             "fieldScience_W",       
+  "attrRatingAvg_M_of_W",  "income_W",              "go_out_W",              "diningAct_W",           "artAct_W",             
+  "go_out_W",              "clubbingAct_W",         "raterDecAvg_W",         "age_W",                 "yogaAct_W",            
+  "raceLatino_W",          "fieldScience_W")
+for(i in 1:length(orderedFeatures)){
+  features = orderedFeatures[1:i]
+  probs = getProbs(myTrain, myTest, features, "dec_M")
+  print(i)
+  print(logLoss(myTest[["dec_M"]], probs),3)
+  
 }
-
-
-for(n in c("womanRateeScore", "simpleWomanRateeScore","manRaterScore","simpleManRaterScore")){
-  print(n)
-  h(myTrain, "dec_M", n)
-}
-
